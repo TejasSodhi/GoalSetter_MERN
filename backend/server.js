394e34331700b79321e13e637f5c742564d342ab
Tejas Sodhi <sodhi.tejas@gmail.com>
2024-02-20 14:36:40 -0500

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("this is server");
+const express = require('express')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const dotenv =require('dotenv').config()
+const colors = require('colors')
+const { connectDB } = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB();
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
